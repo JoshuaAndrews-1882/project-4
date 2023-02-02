3d0b35ed214d2cef4d8c78ffcec10f0ae616845e
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-02 13:22:17 +0000

    Fix DesignConfig and tested a url

--- fandk/urls.py
+++ fandk/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def designs(request):
+    return HttpResponse('Here are the designs')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('designs/', designs, name="designs"),
 ]
