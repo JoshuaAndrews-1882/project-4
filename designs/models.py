f19f1992feaf56a7179990ad5e2e40f1f2a19ea4
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-09 12:03:34 +0000

    Change designs model to str to show the title in admin panel

--- designs/models.py
+++ designs/models.py
@@ -8,3 +8,6 @@ class Design(models.Model):
     description = models.TextField(null=True, blank=True)
     design_link = models.CharField(max_length=2500, null=True, blank=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
