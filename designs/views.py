35883e17e1aca2ded03928e5e0f8a99bdfa9fb84
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-09 14:32:47 +0000

    Created a loop to go through the designs added in admin to show on landing page.  Enabled correct page to show when linked is clicked

--- designs/views.py
+++ designs/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Design
 
 
 def designs(request):
-    return render(request, 'designs/designs.html')
+    designs = Design.objects.all()
+    context = {'designs': designs}
+    return render(request, 'designs/designs.html', context)
 
 
 def design(request, pk):
-    return render(request, 'designs/design.html')
+    design = Design.objects.get(id=pk)
+    return render(request, 'designs/design.html', {'design': design})
