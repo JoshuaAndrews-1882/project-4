c2b526d52b28a298e5c96d1e5e5e29a2a4f6db18
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-09 12:33:22 +0000

    Add rating total and rating ratio to design model

--- designs/models.py
+++ designs/models.py
@@ -8,6 +8,8 @@ class Design(models.Model):
     description = models.TextField(null=True, blank=True)
     design_link = models.CharField(max_length=2500, null=True, blank=True)
     tags = models.ManyToManyField('Tags', blank=True)
+    rating_total = models.IntegerField(default=0, null=True, blank=True)
+    rating_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
