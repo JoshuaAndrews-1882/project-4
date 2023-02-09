07a5a9a5d8ff70622bd0191b6970a04fca9861e3
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-09 12:27:07 +0000

    Create model for user rating the design and leaving a comment.  Also add time stamp to design model

--- designs/models.py
+++ designs/models.py
@@ -7,7 +7,24 @@ class Design(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
     design_link = models.CharField(max_length=2500, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    RATING_TYPE = (
+        ('like', 'Liked'),
+        ('dislike', 'Disliked')
+    )
+    #user = 
+    design = models.ForeignKey(Design, on_delete=models.CASCADE)
+    text = models.TextField(null=True, blank=True)
+    rating = models.CharField(max_length=250, choices=RATING_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
