6a460b8f3297e786ddbeb910ccc4db437fb264e1
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-09 12:00:59 +0000

    Create first model for user designs

--- designs/models.py
+++ designs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class Design(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+    design_link = models.CharField(max_length=2500, null=True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
