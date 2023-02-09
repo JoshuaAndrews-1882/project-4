854d59a8624f6665b317c2190cece394e1e0b96a
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-09 12:31:02 +0000

    Correct spelling on ManyToManyField

--- designs/models.py
+++ designs/models.py
@@ -7,7 +7,7 @@ class Design(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
     design_link = models.CharField(max_length=2500, null=True, blank=True)
-    tags = models.ManyToManyFields('Tags', blank=True)
+    tags = models.ManyToManyField('Tags', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
