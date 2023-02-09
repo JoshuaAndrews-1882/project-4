264c3a4bc8d4840c21cdec39e62446dfc2e1f187
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-09 11:29:38 +0000

    Change design.html to the default domain

--- designs/urls.py
+++ designs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('designs/', views.designs, name="designs"),
+    path('', views.designs, name="designs"),
     path('design/<slug:pk>/', views.design, name="design"),
 ]
