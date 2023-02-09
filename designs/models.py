d5f2e5ccfecc79992aa51e7c9e20db7516f6feb3
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2023-02-09 12:28:41 +0000

    Create tag model

--- designs/models.py
+++ designs/models.py
@@ -28,3 +28,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.value
+
+
+class Tags(models.Model):
+    name = models.Charfield(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
